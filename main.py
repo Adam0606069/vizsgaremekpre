de2c61bbeae848c758e7e0db27d09ba50ee263c8
Adam0606069 <horadam2006@gmail.com>
2025-11-07 10:17:32 +0100

    atlagolas

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ print(adatok)
 
 
 #2. Adatok átlaga
+
+szam_db=0
+osszeg=0
+for szam in adatok:
+    osszeg+=szam
+    szam_db+=1
+
+atlag=osszeg/szam_db
+
+print(f"A beolvasott adatok átlaga {atlag}")
+
 #3. Döntsük el, hogy volt-e 4-es
 #4. Keressük meg, hogy volt-e 5-ös
 #5. Hány darab 9-es volt
 #6. Mennyi a legnagyobb beírt szám?
 #7. Hanyadik indexen van a legkisebb elem?
-#8. Páros számok kiírása
+#8. Páros számok kiírása.
